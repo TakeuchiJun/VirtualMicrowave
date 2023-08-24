3072edfea4a2f87d8f4b1be9a0b570af3a669737
TakeuchiJun <j.takeuchi@kotsubu.lsv.jp>
2023-08-24 16:41:10 +0900

    クラス定義へ変更

--- VirtualMicrowave.playground/Contents.swift
+++ VirtualMicrowave.playground/Contents.swift
@@ -1,39 +1,47 @@
 import UIKit
-// 注釈を追加
-//class VirtualMicrowave {
+
+class VirtualMicrowave {
     
-    // ワット数
-    
-    enum wattBottunType {
-        case wattBottun600W
-        case wattBotton500W
+    // 使用可能ワット数定義
+    enum WattBottunType {
+        case power600W
+        case power500W
     }
     
-    //温め時間
     var warmingTime : Int = 10 //分
     var warmingStopOperation = false
+    var wattType: String = "500W"
 
-    
- //   func start(){
-
+    //温めワット数、温め時間カウントダウン
+    func start(wattBottun: WattBottunType){
+        guard warmingTime < 21 else {
+            print("温め時間は20分までです。設定し直してください。")
+            return
+        }
+        
+        switch wattBottun {
+        case .power600W:
+            wattType = "600W"
+        case .power500W:
+            wattType = "500W"
+        }
+        print("温め開始：\(wattType)で\(warmingTime)分温めます。")
+        
         while !warmingStopOperation {
             print("残り時間:\(warmingTime)分")
             warmingTime -= 1
             if warmingTime < 1 {
                 warmingStopOperation = true
+                }
             }
+        print("- 温め終了 -")
         }
-print("- 温め終了 -")
-        
-  //  }
-   
-    
-    
-//}
+   }
 
-//let controller = VirtualMicrowave()
-//start.warmingTime = 10
-//controller.start()
+//電子レンジ操作
+let controller = VirtualMicrowave()
+controller.warmingTime = 20
+controller.start(wattBottun: .power600W)
 
 
 
